aba609f7df195d6daf646765d6b09b8f978a1160
Owen Davies <owen@odavi.es>
2023-12-02 15:19:50 +0000

    Simplify day 2

--- bin/day2-1.pl
+++ bin/day2-1.pl
@@ -14,16 +14,13 @@ my $maxs = {
 my $total = 0;
 while (<$fh>) {
     chomp;
-    my ($id) = $_ =~ /Game (\d+)/;
-    my @games = split /; /;
+    my ($id) = /(\d+):/;
+    my @balls = split /[;,] /;
     my $possible = 1;
-    for my $g (@games) {
-        my @balls = split /, /, $g;
-        for my $b (@balls) {
-            my ($num, $colour) = $b =~ /(\d+) (\w+)/;
-            if ($num > $maxs->{$colour}) {
-                $possible = 0;
-            }
+    for my $b (@balls) {
+        my ($num, $colour) = $b =~ /(\d+) (\w+)/;
+        if ($num > $maxs->{$colour}) {
+            $possible = 0;
         }
     }
 
--- bin/day2-2.pl
+++ bin/day2-2.pl
@@ -7,26 +7,15 @@ my $file = defined $ARGV[0] ? $ARGV[0] : 'inputs/day2';
 $file = "inputs/day2-$file" if $file =~ /test/;
 open(my $fh, '<', $file) or die $!;
 
-my $maxs = {
-    "red" => 12,
-    "green" => 13,
-    "blue" => 14,
-};
-
 my $total = 0;
 while (<$fh>) {
     chomp;
-    my ($id) = $_ =~ /Game (\d+)/;
-    my @games = split /; /;
-
+    my @balls = split /[;,] /;
     my $lims = {};
-    for my $g (@games) {
-        my @balls = split /, /, $g;
-        for my $b (@balls) {
-            my ($num, $colour) = $b =~ /(\d+) (\w+)/;
-            if (!defined $lims->{$colour} || $lims->{$colour} < $num) {
-                $lims->{$colour} = $num;
-            }
+    for my $b (@balls) {
+        my ($num, $colour) = $b =~ /(\d+) (\w+)/;
+        if (!defined $lims->{$colour} || $lims->{$colour} < $num) {
+            $lims->{$colour} = $num;
         }
     }
 

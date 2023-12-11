3f19326d94fb51de765eecdfb15c084cc289b3ce
Owen Davies <owen@odavi.es>
2023-12-11 12:29:16 +0000

    Neaten up day 11 a bit more
    Use the idea that we can just expand the rows and columns in the
    distance calculation instead of actually changing the coordinates

--- bin/day11-1.pl
+++ bin/day11-1.pl
@@ -10,81 +10,65 @@ open(my $fh, '<', $file) or die $!;
 my $coords = {};
 my $height = 0;
 my $width = 0;
-my @cols_to_expand;
-my @rows_to_expand;
-
+my $rows_to_expand = {};
 while (<$fh>) {
     chomp;
     
     my @line = split //, $_;
-    if ($height == 0) {
-        @cols_to_expand = (1) x $#line;
-        $width = scalar @line;
-    }
+    $width = scalar @line;
 
-    if (!any { $_ eq '#' } @line) {
-        push @rows_to_expand, $height;
-    }
+    $rows_to_expand->{$height} = 1 if all { $_ eq '.' } @line;
 
     my $x = 0;
     for my $ch (@line) {
-        if ($ch eq '#') {
-            $coords->{$x,$height} = 1;            
-            $cols_to_expand[$x] = 0;
-        }
+        $coords->{$x,$height} = 1 if $ch eq '#';
         $x++;
     }
     $height++;
 }
 
-my $height_bumps = {};
-for my $k (keys %$coords) {
-    my ($x, $y) = split $;, $k;
-    my $bump = 0;
-    for my $r (@rows_to_expand) {
-        $bump++ if $y > $r;
+my $cols_to_expand = {};
+for my $col (0..$width - 1) {
+    my $empty = 1;
+
+    for my $row (0..$height) {
+        if (exists $coords->{$col,$row}) {
+            $empty = 0;
+            last;
+        }
     }
-    $height_bumps->{$k} = $bump;
-}
 
-for my $b (keys %$height_bumps) {
-    my ($x, $y) = split $;, $b;
-    my $bump = $height_bumps->{$b};
-    delete $coords->{$b};
-    $coords->{$x,$y+$bump} = 1;
-}
-
-my $width_bumps = {};
-for my $k (keys %$coords) {
-    my ($x, $y) = split $;, $k;
-    my $bump = 0;
-    for (my $i = 0; $i < scalar @cols_to_expand; $i++) {
-        next if $cols_to_expand[$i] == 0;
-        $bump++ if $x > $i;
+    if ($empty == 1) {
+        $cols_to_expand->{$col} = 1;
     }
-    $width_bumps->{$k} = $bump;
-}
-
-for my $b (keys %$width_bumps) {
-    my ($x, $y) = split $;, $b;
-    my $bump = $width_bumps->{$b};
-    delete $coords->{$b};
-    $coords->{$x+$bump,$y} = 1;
 }
 
 my $total = 0;
-for my $k (keys %$coords) {
-    my ($x1, $y1) = split $;, $k;
+my $seen = {};
+for my $k1 (keys %$coords) {
+    my ($x1, $y1) = split $;, $k1;
     for my $k2 (keys %$coords) {
+        next if exists $seen->{$k1,$k2};
         my ($x2, $y2) = split $;, $k2;
-        next if ($x2 == $x1) && ($y2 == $y1);
         $total += dist($x1, $y1, $x2, $y2);
+        $seen->{$k1,$k2} = 1;
+        $seen->{$k2,$k1} = 1;
     }
 }
-say $total / 2;
+
+say $total;
 
 sub dist {
     my ($x1, $y1, $x2, $y2) = @_;
-    return abs($x1 - $x2) + abs($y1 - $y2);
-}
+    my $d = abs($x1 - $x2) + abs($y1 - $y2);
 
+    for my $row (keys %$rows_to_expand) {
+        $d++ if min($y1, $y2) < $row && $row < max($y1, $y2);
+    }
+
+    for my $col (keys %$cols_to_expand) {
+        $d++ if min($x1, $x2) < $col && $col < max($x1, $x2);
+    }
+
+    return $d;
+}

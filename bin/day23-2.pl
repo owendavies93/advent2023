ca76ef679b5cb5cde0b79e307e62870c919ed70b
Owen Davies <owen@odavi.es>
2023-12-23 15:17:11 +0000

    Add optimised version of day 23
    Still very slow!

--- bin/day23-2.pl
+++ bin/day23-2.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/env perl
 use Mojo::Base -strict;
 
+use Data::Dumper;
 use List::AllUtils qw(:all);
+use Memoize;
 use Storable qw(dclone);
 
 my $file = defined $ARGV[0] ? $ARGV[0] : 'inputs/day23';
@@ -19,9 +21,72 @@ while (<$fh>) {
     $height++;
 }
 
+my $adj_graph = {};
+
 my @dirs = (
     [0,-1],[1,0],[0,1],[-1,0]
 );
+
+for (my $y = 0; $y < $height; $y++) {
+    for (my $x = 0; $x < $width ; $x++) {
+        my $e = $grid[$y * $width + $x];
+        next if $e eq '#';
+
+        my $adj = {};
+        for (my $i = 0; $i < scalar @dirs; $i++) {
+            my ($dx, $dy) = @{$dirs[$i]};
+            my $nx = $x + $dx;
+            my $ny = $y + $dy;
+            next if $nx < 0 || $ny < 0 || $nx >= $width || $ny >= $height;
+            
+            my $ne = $grid[$ny * $width + $nx];
+            next if $ne eq '#';
+
+            $adj->{$nx,$ny} = 1;
+        }
+
+        $adj_graph->{$x,$y} = $adj;
+    }
+}
+
+my $keep = {};
+for my $k (keys %$adj_graph) {
+    my $adj = $adj_graph->{$k};
+    if (scalar keys %$adj > 2) {
+        $keep->{$k} = 1;
+    }
+}
+$keep->{1,0} = 1;
+$keep->{$width - 2, $height - 1} = 1;
+
+my $final_adjs = {};
+
+for my $k (keys %$keep) {
+    my @q = ($k);
+    my $seen = { $k => 1 };
+    my $dist = 0;
+
+    while (@q) {
+        my @nq = ();
+        $dist++;
+        
+        for my $c (@q) {
+            my $adj = $adj_graph->{$c};
+            for my $a (keys %$adj) {
+                if (!$seen->{$a}) {
+                    if ($keep->{$a}) {
+                        $final_adjs->{$k}->{$a} = $dist;
+                        $seen->{$a} = 1;
+                    } else {
+                        $seen->{$a} = 1;
+                        push @nq, $a;
+                    }
+                }
+            }
+        }
+        @q = @nq;
+    }
+}
 
 my @q;
 my $s = {};
@@ -39,34 +104,16 @@ while (@q) {
         next;
     }
 
-    for my $adj (adjs($x, $y, $seen)) {
-        my ($nx, $ny) = @$adj;
+    for my $adj (keys %{$adj_graph->{$x,$y}}) {
+        my ($nx, $ny) = split $;, $adj;
+        next if $seen->{$nx,$ny};
         my $ns = dclone($seen);
         $ns->{$nx,$ny} = 1;
-        push @q, [$nx, $ny, $ns, $len + 1];
+        my $l = $adj_graph->{$x,$y}->{$adj};
+        push @q, [$nx, $ny, $ns, $len + $l];
     }
+
 }
 
 say max @lens;
-
-sub adjs {
-    my ($x, $y, $seen) = @_;
-
-    my $e = $grid[$y * $width + $x];
-
-    my @adj;
-    for (my $i = 0; $i < scalar @dirs; $i++) {
-        my ($dx, $dy) = @{$dirs[$i]};
-        my $nx = $x + $dx;
-        my $ny = $y + $dy;
-        next if $seen->{$nx,$ny};
-        
-        my $ne = $grid[$ny * $width + $nx];
-        next if $ne eq '#';
-
-        push @adj, [$nx, $ny];
-    }
-
-    return @adj;
-}
 
